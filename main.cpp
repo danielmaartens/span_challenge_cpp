be4a504b66163efae42cb103591f89c0d4bfae79
Daniel Maartens <daniel@touchfoundry.co.za>
2019-06-25 01:29:12 +0200

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    cout << "Hello World" << "\n";
+
+    system("read -p 'Press Enter to continue...' var");
+
+    return 0;
+}
+
---
+++ test/main.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Daniel Maartens on 2019-06-25.
+//
+
