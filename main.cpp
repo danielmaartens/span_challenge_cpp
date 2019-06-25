af3d7fb6ff7fb3e8e7af79a72121fa6f1b316975
Daniel Maartens <daniel@touchfoundry.co.za>
2019-06-26 00:48:03 +0200

    Added comments

--- main.cpp
+++ main.cpp
@@ -65,6 +65,29 @@ struct MatchPointComparator {
     }
 };
 
+/**
+ * Converts a vector to a list.
+ * This is useful because lists can be easiler sorted through
+ * by using Comparator classes.
+ *
+ * @param vector
+ * @return
+ */
+list<TeamValue> vectorToList(vector<TeamValue> vector) {
+    list<TeamValue> list;
+
+    for (TeamValue v : vector) {
+        list.push_back(v);
+    }
+
+    return list;
+}
+
+/**
+ * Converts expected user input for yes/no/continue questions into a boolean value.
+ * @param s
+ * @return
+ */
 int booleanFromString(string s) {
 
     transform(s.begin(), s.end(), s.begin(), ::tolower);
@@ -78,21 +101,21 @@ int booleanFromString(string s) {
     }
 }
 
-list<TeamValue> vectorToList(vector<TeamValue> vector) {
-    list<TeamValue> list;
-
-    for (TeamValue v : vector) {
-        list.push_back(v);
-    }
-
-    return list;
-}
-
+/**
+ * Check's if the file inputted by the user is valid/exists
+ * @param name
+ * @return
+ */
 bool fileExists(const string &name) {
     struct stat buffer;
     return (stat(name.c_str(), &buffer) == 0);
 }
 
+/**
+ * Sets the rank value for all teams.
+ * Note: the list must be sorted.
+ * @param sortedTeamValues
+ */
 void setTeamRanks(list<TeamValue> &sortedTeamValues) {
 
     int index = 1;
@@ -118,6 +141,17 @@ void setTeamRanks(list<TeamValue> &sortedTeamValues) {
     }
 }
 
+/**
+ * Expects a string containing the name of the team followed by a space and then the team's score for that match.
+ * E.g. team "GoGetters" with score 10 should have a string as follows: "GoGetters 10"
+ *
+ * It will then convert this string into a TeamValue object that has a name and value variable.
+ * It should also convert the string score into a number.
+ *
+ * @param s
+ * @param pattern
+ * @return
+ */
 TeamValue getTeamResultFromString(const string &s, const string &pattern) {
 
     regex r(pattern);
@@ -129,15 +163,21 @@ TeamValue getTeamResultFromString(const string &s, const string &pattern) {
 
     string team = m[1];
 
-    // Remove the last space from the team name
+    // Remove the space at the end of the team name
     string name = team.substr(0, team.size() - 1);
 
+    // Convert string value into int type.
     int value = stoi(m[2]);
 
+    // return a TeamValue class object
     return TeamValue(name, value);
-
 }
 
+/**
+ * Convert a map object to a list for processing of data later.
+ * @param teamValuesMap
+ * @return
+ */
 vector<TeamValue> convertTeamValueMapToList(map<string, int> teamValuesMap) {
     vector<TeamValue> list;
 
@@ -146,15 +186,23 @@ vector<TeamValue> convertTeamValueMapToList(map<string, int> teamValuesMap) {
         list.push_back(team);
     }
 
-    return list;
-}
-
+/**
+ * Processes a vector of the two team scores in a single match
+ * and returns a new TeamValue object where the value parameter
+ * represents the points the team received from either Losing/Winning/Drawing the match.
+ *
+ * @param matchResults
+ * @return
+ */
 vector<TeamValue> calculateMatchPoints(const vector<TeamValue> &matchResults) {
 
     vector<TeamValue> matchPoints;
 
     TeamValue teamA = matchResults[0];
     TeamValue teamB = matchResults[1];
+
+    // Initialise new TeamValue objects for each team
+    // setting initial points to 0
 
     string teamAName = teamA.getName();
     int teamAGoals = teamA.getValue();
@@ -164,24 +212,42 @@ vector<TeamValue> calculateMatchPoints(const vector<TeamValue> &matchResults) {
     int teamBGoals = teamB.getValue();
     TeamValue teamBPoints = TeamValue(teamBName, 0);
 
+    // Match is a DRAW
     if (teamAGoals == teamBGoals) {
 
         teamAPoints.setValue(1);
         teamBPoints.setValue(1);
 
+    // Team A WON
     } else if (teamAGoals > teamBGoals) {
         teamAPoints.setValue(3);
+
+    // Team B WON
     } else {
         teamBPoints.setValue(3);
     }
 
+    // Add the new objects to an empty vector
     matchPoints.push_back(teamAPoints);
     matchPoints.push_back(teamBPoints);
 
     return matchPoints;
 }
 
+/**
+ * When this function is called we have a vector
+ * containing each team's match points for all games played.
+ *
+ * Here we want to reduced that vector to one that only has
+ * one entry for each team, with each new object having it's
+ * value represent the sum of all match points gained in the league.
+ *
+ * @param allTeamMatchPoints
+ * @return
+ */
 vector<TeamValue> reduceTeamMatchPoints(const vector<TeamValue> allTeamMatchPoints) {
+
+    // Usage of a map here makes it easier to reduce into single entries per team.
     map<string, int> finalTeamPoints;
 
     for (TeamValue matchPoints : allTeamMatchPoints) {
@@ -189,56 +255,100 @@ vector<TeamValue> reduceTeamMatchPoints(const vector<TeamValue> allTeamMatchPoin
         string name = matchPoints.getName();
         int points = matchPoints.getValue();
 
+        // If the name does not exist in the map, it will be initialised with the value of points.
+        // Otherwise it will just add this match's points to the previous points value.
         finalTeamPoints[name] += points;
     }
 
+    // Convert the map back into a list for better processing later.
     return convertTeamValueMapToList(finalTeamPoints);
 }
 
-vector<string> splitResultsLineIntoVector(string resultLine, const string delimiter) {
-    vector<string> result;
-    size_t pos = resultLine.find(delimiter);
-    string token = resultLine.substr(0, pos);
-    result.push_back(token);
-    resultLine.erase(0, pos + delimiter.length());
-    result.push_back(resultLine);
 
-    return result;
-}
-
-list<TeamValue> getOrderedMatchPointsFromFile(const string file) {
+/**
+ * This is the most important function.
+ * It serves as the parent for most of the other functions within this module.
+ * It is responsible for reading through the file contents line by line and
+ * processing the final results of the league based on all the matches played.
+ *
+ * @param file
+ * @return
+ */
+list<TeamValue> getLeagueResults(const string file) {
     vector<TeamValue> allTeamMatchPoints;
     vector<TeamValue> finalTeamMatchPoints;
 
+    // read file contents
     ifstream infile(file);
     string line;
 
+    // go through each line of the file
     while (getline(infile, line)) {
 
         vector<TeamValue> scores;
+
+        // Each line represents the outcome of a match.
+        // Each team's own outcome of the match is separated by a ", "
+        // which is why we first split the line by ", " to get a matchResults vector
+        // of two strings representing the outcome of each team for the match.
         vector<string> matchResults = splitResultsLineIntoVector(line, ", ");
 
+        // Now we loop through the matchResults
         for (string result : matchResults) {
 
+            // We parse the string into a TeamValue object for easy processing later.
             TeamValue teamResult = getTeamResultFromString(result, TEAM_RESULT_GROUPING_PATTERN);
+
+            // We add this result to a vector representing the scores for each team of this match.
             scores.push_back(teamResult);
         }
 
+        // Now that we have a vector of TeamValue objects for the match representing each team,
+        // We can calculate the match points.
         vector<TeamValue> matchPoints = calculateMatchPoints(scores);
+
+        // Here we concatenate the new matchPoints vector with all previous added matchPoints.
+        // The purpose of this is to have a vector of TeamValue objects each representing
+        // the points the team gained in a match.
         allTeamMatchPoints.insert(allTeamMatchPoints.end(), matchPoints.begin(), matchPoints.end());
 
     }
 
+    // Now we reduce this vector of all our teams' matchPoints into a vector
+    // containing a single entry for each team with the value representing the sum of all their match points gained.
     finalTeamMatchPoints = reduceTeamMatchPoints(allTeamMatchPoints);
 
+    // Convert our finalTeamMatchPoints vector to a list so that we can
+    // easily sort our data with a custom Comparator.
     list<TeamValue> teamMatchPointsList = vectorToList(finalTeamMatchPoints);
 
+    // Sort by points DESC, and then by name ASC.
     teamMatchPointsList.sort(MatchPointComparator());
+
+    // Set the team ranks on the sorted data.
     setTeamRanks(teamMatchPointsList);
 
     return teamMatchPointsList;
 }
 
+
+/**
+ * This Print class serves as an easy more DRY way
+ * of delaying output to the console.
+ *
+ * In the beginning we want to simulate the console talking to you
+ * and giving you enough time to read the messages.
+ *
+ * This class serves to make the process of accomplishing that
+ * much more easily.
+ *
+ * The class is instantiated with an initialDelay.
+ * Once the print.delayed() function is invoked the initialDelay will be added to the current runningDelay.
+ *
+ * Every time the print.delayed() is invoked it uses the runningDelay property to figure out how long it should sleep for.
+ *
+ * So every subsequent call to the function will always result in an even delay between subsequent outputs.
+ */
 class Print {
 
 private:
@@ -279,12 +389,15 @@ int main() {
         print.delayed("\nPlease provide the full path of the file where your results are stored:\n");
         print.reset(0);
 
-        cin >> file;
-        cin.get();
+        cin >> file; // read in user input and store it in the file variable
+        cin.get(); // cin.get() is called so that we can wait for the user to input before going on with the program.
 
+        // Does file exist ?
         if (fileExists(file)) {
 
-            list<TeamValue> matchPoints = getOrderedMatchPointsFromFile(file);
+            // It does so let's start processing
+            // process the file contents and get the league results
+            list<TeamValue> matchPoints = getLeagueResults(file);
 
             print.ln("\nRESULTS\n");
 
@@ -301,6 +414,8 @@ int main() {
 
             answerYes = booleanFromString(userInput);
 
+
+            // if the answerYes == -1 that means their input did not match any of our options. So we repeat the question until they do.
             while (answerYes == -1) {
                 print.ln("\nI do not understand your command, please try again...");
                 print.ln("Would you like to check match point results of another league ? [y/n]: ");
